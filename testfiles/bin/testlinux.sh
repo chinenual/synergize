edc9f7af585412fbf9bf2545e3eb05f52f8865d5
Steve Tynor <chinenual@gmail.com>
2020-05-04 17:06:44 -0400

    run tests at 19200

--- testfiles/bin/testlinux.sh
+++ testfiles/bin/testlinux.sh
@@ -2,4 +2,4 @@
 
 iterations=${1:-20}
 
-bash testfiles/bin/serialtests.sh ${iterations} output/linux-amd64/Synergize -port /dev/ttyS0 2>&1 | tee serialtests-linux.log
+bash testfiles/bin/serialtests.sh ${iterations} output/linux-amd64/Synergize -port /dev/ttyS0 -baud 19200 2>&1 | tee serialtests-linux.log
