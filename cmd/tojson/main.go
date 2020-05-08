ae8fc8784a7d1660bfb836382b1578d8a5227290
Steve Tynor <chinenual@gmail.com>
2020-05-08 16:36:14 -0400

    new cmd packages for utility progs

---
+++ cmd/tojson/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"fmt"
+	"log"
+	"path"
+	"strings"
+	
+	"github.com/chinenual/synergize/data"
+)
+
+func jsonize (filename string) {
+	ext := strings.ToLower(path.Ext(filename))
+	switch ext {
+	case ".vce":
+		vce,err := data.VceReadFile(filename)
+		if err != nil {
+			log.Panic(err);
+		}
+		
+		b,_ := json.MarshalIndent(vce, "", " ")
+		result := string(b)
+		
+		fmt.Println(result)
+	case ".crt":
+		crt,err := data.CrtReadFile(filename)
+		if err != nil {
+			log.Panic(err);
+		}
+		
+		b,_ := json.MarshalIndent(crt, "", " ")
+		result := string(b)
+		
+		fmt.Println(result)
+	default:
+		log.Panicf("ERROR: don't know what to do with %s\n", filename)
+	}
+}
+
+func main() {
+	for _, a := range os.Args[1:] {
+		jsonize(a)
+	}
+}
