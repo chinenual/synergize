b84d2ce52c19a8673406251fff10ec412b7b4b6b
Steve Tynor <chinenual@gmail.com>
2020-06-11 21:43:28 -0400

    use the boilerplate i created for the astilectron demo project

--- uitest/test/hooks.js
+++ uitest/test/hooks.js
@@ -2,27 +2,85 @@ const Application = require('spectron').Application;
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const electron = require('electron');
-const {exec} = require("child_process");
+const { exec } = require("child_process");
+
+const APPNAME = 'Synergize';
+const PORT = 55555; // the port the main process will listen to
 
 global.before(() => {
   chai.should();
   chai.use(chaiAsPromised);
 });
 
+// Map nodejs arch to golang arch
+let archMap = {
+  "arm": "arm",
+  "ia32": "386",
+  "x86": "386",
+  "x64": "amd64",
+  "ia64": "amd64"
+};
+
+if (archMap[process.arch] === undefined) {
+  console.log(`FATAL: unhandled platform/processor type (${process.arch}) - add your variant to archMap in test/hooks.js`);
+  process.exit(1);
+}
+
+function mainExe() {
+  if (process.platform === 'darwin') {
+    return `../output/darwin-amd64/${APPNAME}.app/Contents/MacOS/${APPNAME}`;
+  } else if (process.platform === 'linux') {
+    return `../output/linux-${archMap[process.arch]}/${APPNAME}`;
+  } else if (process.platform === 'win32') {
+    return `../output/windows-${archMap[process.arch]}/${APPNAME}.exe`;
+  } else {
+    console.log("FATAL: unhandled platform/os - add your variant here");
+    process.exit(1);
+  }
+}
+
+function electronExe() {
+  if (process.platform === 'darwin') {
+    return `../output/darwin-amd64/${APPNAME}.app/Contents/MacOS/vendor/electron-darwin-amd64/${APPNAME}.app/Contents/MacOS/${APPNAME}`;
+  } else if (process.platform === 'linux') {
+    return `../output/linux-${archMap[process.arch]}/vendor/electron-linux-${archMap[process.arch]}/electron`;
+  } else if (process.platform === 'win32') {
+    return `${process.env.APPDATA}/${APPNAME}/vendor/electron-windows-${archMap[process.arch]}/Electron.exe`;
+  } else {
+    console.log("FATAL: unhandled platform - add your variant here");
+    process.exit(1);
+  }
+}
+
+function astilectronJS() {
+  if (process.platform === 'darwin') {
+    return `../output/darwin-amd64/${APPNAME}.app/Contents/MacOS/vendor/astilectron/main.js`;
+  } else if (process.platform === 'linux') {
+    return `../output/linux-${archMap[process.arch]}/vendor/vendor/astilectron/main.js`;
+  } else if (process.platform === 'win32') {
+    return `${process.env.APPDATA}/${APPNAME}/vendor/astilectron/main.js`;
+  } else {
+    console.log("FATAL: unhandled platform - add your variant here");
+    process.exit(1);
+  }
+}
+
 module.exports = {
   async startMainApp() {
-     exec("../output/darwin-amd64/Synergize.app/Contents/MacOS/Synergize -UITEST 55555", (error, stdout, stderr) => {
-    if (error) {
+    console.log(`node arch: "${process.arch}"   golang arch: "${archMap[process.arch]}"`)
+    console.log(`Starting main exe: ${mainExe()}`);
+    exec(`"${mainExe()}" -UITEST ${PORT}`, (error, stdout, stderr) => {
+      if (error) {
         console.log(`error: ${error.message}`);
         return;
-    }
-    if (stderr) {
+      }
+      if (stderr) {
         console.log(`stderr: ${stderr}`);
         return;
-    }
-    console.log(`stdout: ${stdout}`);
+      }
+      console.log(`stdout: ${stdout}`);
 
-  });
+    });
   },
 
   async getApp() {
@@ -31,15 +89,16 @@ module.exports = {
 
   async startApp() {
     module.exports.startMainApp();
-   
-    const rendererApp = await new Application({
-	
-	path: '../output/darwin-amd64/Synergize.app/Contents/MacOS/vendor/electron-darwin-amd64/Synergize.app/Contents/MacOS/Synergize',
-	args: ['../output/darwin-amd64/Synergize.app/Contents/MacOS/vendor/astilectron/main.js', '127.0.0.1:55555', 'true' ],
 
-	
-        chromeDriverLogPath: './chromedriver.log',
-        webdriverLogPath: './webdriver.log'
+    console.log(`Starting electron exe: ${electronExe()}`);
+    const rendererApp = await new Application({
+
+      path: electronExe(),
+      args: [astilectronJS(), `127.0.0.1:${PORT}`, 'true'],
+
+      // for debugging:
+      //chromeDriverLogPath: './chromedriver.log',
+      //webdriverLogPath: './webdriver.log'
 
     }).start();
     chaiAsPromised.transferPromiseness = rendererApp.transferPromiseness;
