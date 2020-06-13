2efbc4731fa7c6da43575d1d28da672452814c3b
Steve Tynor <chinenual@gmail.com>
2020-06-13 15:41:05 -0400

    initvram test

---
+++ uitest/test/page-objects/voice-INITVRAM.js
@@ -0,0 +1,37 @@
+module.exports = {
+  name: '',
+  nOsc: {value: '1'},
+  keysPlayable: '32',
+  vibType: 'Sine',
+  VIBDEP: {value: '0'},
+  VIBRAT: {value: '16'},
+  VIBDEL: {value: '0'},
+  APVIB: {value: '32'},
+  VACENT: {value: '24'},
+  VASENS: {value: '0'},
+  VTCENT: {value: '0'},
+  VTSENS: {value: '0'},
+  VTRANS: {value: '0'},
+  nFilter: '0',
+  patchType: {value: '0'},
+  
+  "patchFOInputDSR[1]": '',
+  "patchFOInputDSR[2]": {exist: false},
+
+  "patchAdderInDSR[1]": '1',
+
+  "patchOutputDSR[1]": '1',
+
+  "OHARM[1]": {value: '1'},
+  "OHARM[2]": {exist: false},
+  
+  "FDETUN[1]": {value: '0'},
+
+  "wkWAVE[1]": {value: 'Sin'},
+
+  "wkKEYPROP[1]": {selected: false},
+
+  "FILTER[1]": {value: ''},
+
+
+};
