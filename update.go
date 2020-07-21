008f8d8dc9f47b2d2fe857359a3eeb530b06babc
Steve Tynor <chinenual@gmail.com>
2020-07-21 15:15:18 -0400

    cleanup lint warnings

--- update.go
+++ update.go
@@ -12,14 +12,14 @@ import (
 var newVersionAvailable bool = false
 var checkedForVersion bool = false
 
-var versionUrl string = "https://api.chinenual.com/api/v1/version?latest&app=Synergize&v="+Version+"&OS=" + runtime.GOOS + "&ARCH=" + runtime.GOARCH
+var versionUrl string = "https://api.chinenual.com/api/v1/version?latest&app=Synergize&v=" + Version + "&OS=" + runtime.GOOS + "&ARCH=" + runtime.GOARCH
 
 type GithubReleaseApiResponse []struct {
 	Redirect string `json:"redirect"`
-	TagName string `json:"tag_name"`
+	TagName  string `json:"tag_name"`
 }
 
-func getLatest(url string) (version string,err error) {
+func getLatest(url string) (version string, err error) {
 	var response *http.Response
 	if response, err = http.Get(url); err != nil {
 		log.Printf("Check for new version API call failed with %v\n", err)
@@ -35,11 +35,9 @@ func getLatest(url string) (version string,err error) {
 				log.Printf("could not decode response %v\n", err)
 				return
 			} else if res[0].Redirect != "" {
-				version,err = getLatest(res[0].Redirect)
-				return
+				version, err = getLatest(res[0].Redirect)
 			} else {
-				version = res[0].TagName;
-				return
+				version = res[0].TagName
 			}
 		}
 	}
@@ -55,15 +53,15 @@ func CheckForNewVersion(forceRecheck bool, connected bool) (newVersion bool) {
 		if forceRecheck {
 			url = versionUrl + "&connected=" + strconv.FormatBool(connected)
 		}
-		if latestVersion,err = getLatest(url); err != nil {
-			log.Printf("Error checking for new version: %v",err)
+		if latestVersion, err = getLatest(url); err != nil {
+			log.Printf("Error checking for new version: %v", err)
 			return
-		}			
+		}
 		log.Printf("Latest version is %s\n", latestVersion)
 		if "v"+Version != latestVersion {
 			newVersionAvailable = true
 		}
 	}
-	newVersion = newVersionAvailable;
+	newVersion = newVersionAvailable
 	return
 }
