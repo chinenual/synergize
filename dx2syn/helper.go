940691e21a54254a184ed01aa5a1f17b4236495a
Steve Tynor <chinenual@gmail.com>
2020-12-21 12:14:49 -0500

    helper functions for unscaling envelope values to stored byte equivs

--- dx2syn/helper.go
+++ dx2syn/helper.go
@@ -24,8 +24,8 @@ func helperBlankVce() (vce data.VCE, err error) {
 	}
 	for i := 0; i < 16; i++ {
 		// now re-allocate each envelope to their max possible length:
-		vce.Envelopes[i].AmpEnvelope.Table = make([]byte, 4 * 16)
-		vce.Envelopes[i].FreqEnvelope.Table = make([]byte, 4 * 16)
+		vce.Envelopes[i].AmpEnvelope.Table = make([]byte, 4*16)
+		vce.Envelopes[i].FreqEnvelope.Table = make([]byte, 4*16)
 	}
 	return
 }
@@ -56,11 +56,70 @@ func helperVCEToJSON(vce data.VCE) (result string) {
 	// compact the vce before printing it:
 	var err error
 	var compacted data.VCE
-	if compacted,err = helperCompactVCE(vce); err != nil {
+	if compacted, err = helperCompactVCE(vce); err != nil {
 		return fmt.Sprintf("ERROR: %v", err)
 	}
 
 	b, _ := json.MarshalIndent(compacted, "", "\t")
 	result = string(b)
 	return
+}
+
+// translations of the javascript functions in viewVCE_envs.js
+
+var _freqTimeScale = []int{0, 1, 2, 3, 4, 5, 6, 7,
+	8, 9, 10, 11, 12, 13, 14, 15,
+	25, 28, 32, 36, 40, 45, 51, 57,
+	64, 72, 81, 91, 102, 115, 129, 145,
+	163, 183, 205, 230, 258, 290, 326, 366,
+	411, 461, 517, 581, 652, 732, 822, 922,
+	1035, 1162, 1304, 1464, 1644, 1845, 2071, 2325,
+	2609, 2929, 3288, 3691, 4143, 4650, 5219, 5859,
+	6576, 7382, 8286, 9300, 10439, 11718, 13153, 14764,
+	16572, 18600, 20078, 23436, 26306, 29528, 29529, 29530,
+	29531, 29532, 29533, 29534, 29535}
+var _ampTimeScale = []int{0, 1, 2, 3, 4, 5, 6, 7,
+	8, 9, 10, 11, 12, 13, 14, 15,
+	16, 17, 18, 19, 20, 21, 22, 23,
+	24, 25, 26, 27, 28, 29, 30, 31,
+	32, 33, 34, 35, 36, 37, 38, 39,
+	40, 45, 51, 57, 64, 72, 81, 91,
+	102, 115, 129, 145, 163, 183, 205, 230,
+	258, 290, 326, 366, 411, 461, 517, 581,
+	652, 732, 822, 922, 1035, 1162, 1304, 1464,
+	1644, 1845, 2071, 2325, 2609, 2929, 3288, 3691,
+	4143, 4650, 5219, 5859, 6576}
+
+// translate a frequency time "as displayed" to "byte value as stored"
+func helperUnscaleFreqTimeValue(time int) byte {
+	// fixme: linear search is brute force - but the list is short - performance is "ok" as is...
+	for i, v := range _freqTimeScale {
+		if v >= time {
+			return byte(i)
+		}
+	}
+	// shouldnt happen!
+	return byte(len(_freqTimeScale))
+}
+
+// translate a amplitude time "as displayed" to "byte value as stored"
+func helperUnscaleAmpTimeValue(time int) byte {
+	// fixme: linear search is brute force - but the list is short - performance is "ok" as is...
+	for i, v := range _ampTimeScale {
+		if v >= time {
+			return byte(i)
+		}
+	}
+	// shouldnt happen!
+	return byte(len(_ampTimeScale))
+}
+
+// translate a frequency value "as displayed" to "byte value as stored"
+func helperUnscaleFreqEnvValue(val byte) byte {
+	return val
+}
+
+// translate a amplitude value "as displayed" to "byte value as stored"
+func UnscaleAmpEnvValue(val byte) byte {
+	return val + 55
 }
