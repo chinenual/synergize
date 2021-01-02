db773a50351d5e71fab36a9e918f0cb7cc2a1ac5
Steve Tynor <chinenual@gmail.com>
2021-01-02 16:06:27 -0500

    fix expect values

--- dx2syn/rates_test.go
+++ dx2syn/rates_test.go
@@ -5,11 +5,11 @@ import (
 )
 
 func TestComputeDurationsMs(t *testing.T) {
-	expect := [4]int{6, 7, 7, 45}
+	expect := [4]int{1478, 2, 0, 54312}
 	actual := computeDurationsMs([4]int{99, 50, 40, 0}, [4]int{30, 99, 99, 10})
 	for i, exp := range expect {
 		if actual[i] != exp {
-			t.Errorf("ms[%d] expected %d, got '%d'\n", i, exp, actual[0])
+			t.Errorf("ms[%d] expected %d, got '%d'\n", i, exp, actual[i])
 		}
 	}
 	return
@@ -20,18 +20,18 @@ func TestComputeDurationsMs1(t *testing.T) {
 	actual := computeDurationsMs([4]int{99, 50, 40, 0}, [4]int{30, 99, 99, 10})
 	for i, exp := range expect {
 		if actual[i] != exp {
-			t.Errorf("ms[%d] expected %d, got '%d'\n", i, exp, actual[0])
+			t.Errorf("ms[%d] expected %d, got '%d'\n", i, exp, actual[i])
 		}
 	}
 	return
 }
 
 func TestComputeDurationsMs2(t *testing.T) {
-	expect := [4]int{2, 41, 13, 844}
+	expect := [4]int{2, 42, 13, 845}
 	actual := computeDurationsMs([4]int{99, 95, 60, 0}, [4]int{90, 50, 80, 50})
 	for i, exp := range expect {
 		if actual[i] != exp {
-			t.Errorf("ms[%d] expected %d, got '%d'\n", i, exp, actual[0])
+			t.Errorf("ms[%d] expected %d, got '%d'\n", i, exp, actual[i])
 		}
 	}
 	return
