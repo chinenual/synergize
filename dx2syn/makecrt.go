f42d17b8447acf69ae3eee1dbe52d617e417f142
Steve Tynor <chinenual@gmail.com>
2021-02-13 16:30:25 -0500

    create extra CRTs if cant fit all into one

--- dx2syn/makecrt.go
+++ dx2syn/makecrt.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"github.com/chinenual/synergize/data"
+	"github.com/orcaman/writerseeker"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
+func checkWriteCrtFromVCEArray(vces []*data.VCE) (err error) {
+	var writebuf = writerseeker.WriterSeeker{}
+
+	if err = data.WriteCrt(&writebuf, vces); err != nil {
+		return
+	}
+	return
+}
+
 func makeCrt(dirPath string) (err error) {
 
+	crtCount := 1
 	crtPath := filepath.Join(dirPath, filepath.Base(dirPath)+".CRT")
 	var vces []*data.VCE
 
@@ -21,22 +35,36 @@ func makeCrt(dirPath string) (err error) {
 	for _, f := range fileInfos {
 		path := filepath.Join(dirPath, f.Name())
 		if strings.EqualFold(filepath.Ext(path), ".vce") {
-			if len(vces) < 24 {
-				var vce data.VCE
-				if vce, err = data.ReadVceFile(path); err != nil {
-					return err
-				} else {
-					vces = append(vces, &vce)
-					log.Printf("Add %s ...\n", path)
-				}
+			var vce data.VCE
+			if vce, err = data.ReadVceFile(path); err != nil {
+				return err
 			} else {
-				log.Printf("WARNING: ignore extra VCEs in folder: %s\n", path)
+				// will it fit?
+				newVces := append(vces, &vce)
+				if err = checkWriteCrtFromVCEArray(newVces); err != nil {
+					// doesn't fit.  Write what we have to the current crtPath, then start a new
+					// list of voices for a new crtPath
+					if len(vces) < 1 {
+						return errors.New("Must have at least one VCE file")
+					}
+					// write the last set of vce's that fit:
+					if err = data.WriteCrtFileFromVCEArray(crtPath, vces); err != nil {
+						fmt.Printf("diag %v \n", vces)
+						return
+					}
+					// initialize the new list of vces to just this next voice
+					newVces = nil
+					newVces = append(newVces, &vce)
+					// start a new file
+					crtCount += 1
+					crtPath = filepath.Join(dirPath, filepath.Base(dirPath)+"-"+strconv.Itoa(crtCount)+".CRT")
+					log.Printf("%s: Add %s ...\n", crtPath, path)
+				} else {
+					log.Printf("%s: Add %s ...\n", crtPath, path)
+				}
+				vces = newVces
 			}
 		}
-	}
-
-	if err = data.WriteCrtFileFromVCEArray(crtPath, vces); err != nil {
-		return
 	}
 	return
 }
