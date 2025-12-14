8fbff5b3b6cda047ad456c12b88848784260f017
Steve Tynor <chinenual@gmail.com>
2025-12-13 20:23:44 -0500

    typescript appium/mocha starting point

--- uitest-ts-appium-mocha/test/specs/test.e2e.ts
+++ uitest-ts-appium-mocha/test/specs/test.e2e.ts
@@ -1,7 +1,8 @@
-import { expect, $ } from '@wdio/globals'
+import { expect, driver, $ } from '@wdio/globals'
 
 describe('Synergize Testing', () => {
     it('basics', async function () {
+        await expect(driver.getTitle().toHaveText('Synergize'));
         await expect($('~synergyName').toHaveText('not connected'));
         await expect($('~controlSurfaceName').toHaveText(''));
     })
