5d6585f5c6e2aaa7ab583d33c0e6ede13bc3ee26
Steve Tynor <chinenual@gmail.com>
2025-12-16 20:37:08 -0500

    new -CWD option to override appium tmpdir; remove older test-oriented command line options no longer used in the wails app

--- uitest-java/src/test/java/com/chinenual/synergize/IntegrationTestSuite.java
+++ uitest-java/src/test/java/com/chinenual/synergize/IntegrationTestSuite.java
@@ -5,6 +5,7 @@ import io.appium.java_client.mac.Mac2Driver;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 
 import java.io.File;
+import java.time.Duration;
 import org.junit.jupiter.api.ClassOrderer;
 import org.junit.jupiter.api.TestClassOrder;
 import org.junit.platform.suite.api.AfterSuite;
@@ -30,8 +31,11 @@ public class IntegrationTestSuite {
             System.out.println("Started Appium service: " + service.getUrl());
 
             DesiredCapabilities capabilities = new DesiredCapabilities();
+            // App path must be absolute - appium looks for relative paths relative to the driver tmp directory
             String path = new File("../bin/Synergize.app").getCanonicalPath();
-            String[] args = {"-MOCKSYNIO", "-SERIALVERBOSE"};
+            // Appium will run the app in an appium driver specific tmp directory.
+            // We need the app to run locally so it can find test files via relative paths:
+            String[] args = {"-MOCKSYNIO", "-SERIALVERBOSE", "-CWD", System.getProperty("user.dir")};
             System.out.println("APP PATH: " + path);
             capabilities.setCapability("appium:appPath", path);
             capabilities.setCapability("appium:arguments", args);
@@ -51,6 +55,12 @@ public class IntegrationTestSuite {
 
     @AfterSuite
     public static void teardown() {
+        System.err.println("SLEEPING 20 min");
+        try {
+            Thread.sleep(Duration.ofMinutes(20));
+        } catch (InterruptedException ex) {
+            System.getLogger(IntegrationTestSuite.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
         if (driver != null) {
             driver.quit();
         }
