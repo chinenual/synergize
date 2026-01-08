acf516e2f06038d66e4170d1608b4758847ab5fa
Steve Tynor <chinenual@gmail.com>
2026-01-08 11:07:38 -0500

    fix new modal close - use bootstrap attribute

--- frontend/modal.js
+++ frontend/modal.js
@@ -1,22 +1,37 @@
-// Adapted from https://jasonwatmore.com/post/2023/01/04/vanilla-js-css-modal-popup-dialog-tutorial-with-example
+// Adapted from
+// https://jasonwatmore.com/post/2023/01/04/vanilla-js-css-modal-popup-dialog-tutorial-with-example
+// changed to support the old bootstrap classes and attributes.
 
 // open modal by id
 export function openModal(id) {
-    document.getElementById(id).classList.add('open');
-    document.body.classList.add('modal-open');
+  console.log('openModal()', id);
+  let modal = document.getElementById(id);
+  modal.classList.add('open');
+
+  document.body.classList.add('modal-open');
 }
 
 // close currently open modal
 export function closeModal() {
-    document.querySelector('.modal.open').classList.remove('open');
-    document.body.classList.remove('modal-open');
+  console.trace('closeModal()');
+  let modal = document.querySelector('.modal.open');
+  if (modal) {
+    modal.classList.remove('open');
+  }
+  document.body.classList.remove('modal-open');
 }
 
 // redraw - maybe not needed post-bootstrap.  TBD.
 export function updateModal(id) {
+  console.log('updateModal()', id);
 }
 
-/**** 
+let closeButtons = document.querySelectorAll('[data-bs-dismiss=modal]');
+closeButtons.forEach((button) => {
+        button.addEventListener('click', closeModal);
+});
+
+/****
 window.addEventListener('load', function() {
     // close modals on background click
     document.addEventListener('click', event => {
